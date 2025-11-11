41bb19814740d930893892e5e2415d6a6b78124d
Bruno Grassi <brunoograssi@hotmail.com>
2025-11-11 01:44:12 -0300

    create mailTrap

---
+++ src/providers/implementations/MailTrapMailProvider.ts
@@ -0,0 +1,32 @@
+import type { IMailProvider, IMessage } from "../IMailProvider.js";
+import nodemailer from 'nodemailer'
+
+export class MailTrapMailProvider implements IMailProvider{
+    private transporter;
+
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.mailtrap.io',
+            port: 2525,
+            auth: {
+                user: '',
+                pass: ''
+            }
+        })
+    }
+
+    async sendMail(message: IMessage): Promise<void> {
+        await this.transporter.sendMail({
+            to: {
+                name: message.to.name,
+                address: message.to.email
+            },
+            from: {
+                name: message.from.name,
+                address: message.from.email
+            },
+            subject: message.subject,
+            html: message.body
+        })
+    }
+}
