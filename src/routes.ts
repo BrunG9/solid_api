02906eb22535c36f2725104a6310af1db5d8a75f
Bruno Grassi <brunoograssi@hotmail.com>
2025-11-11 01:41:23 -0300

    update post route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { createUserController } from "./useCases/CreateUser/index.js";
 
 const router = Router()
 
 router.post('/users', (req, res) => {
-    return res.status(201).send()
+    return createUserController.handle(req, res)
 })
 
 export { router }
